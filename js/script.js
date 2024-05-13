2861cc62beca5e4de9e101e8f3647a16d9aceb30
Smorareq1 <smorareq1@gmail.com>
2024-05-13 16:58:52 -0600

    TERMINADO

--- js/script.js
+++ js/script.js
@@ -46,70 +46,44 @@ form.addEventListener('submit', function(event) {
     }
 });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-// Mapas personalizados para cada opción
 const mapasPersonalizados = {
     electronicos: {
-        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.917643673955!2d-90.49182828898171!3d14.60376717693958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a3a991475f6f%3A0x3ded42e1475475f0!2sUniversidad%20del%20Valle%20de%20Guatemala!5e0!3m2!1ses!2sgt!4v1712725528732!5m2!1ses!2sgt",
+        url: "https://www.google.com/maps/d/u/0/embed?mid=16vdwoq3Os_TbpwkegZ7SLNoSFXCUMGg&ehbc=2E312F&noprof=1",
         width: 600,
         height: 450
     },
     papel: {
-        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.5871510860193!2d-90.55444148898145!3d14.622578876478581!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a110e16ca3d3%3A0xc69355632110809e!2sMiraflores!5e0!3m2!1ses!2sgt!4v1712726770719!5m2!1ses!2sgt",
+        url: "https://www.google.com/maps/d/u/0/embed?mid=1zreIZLdoUIIrq5eFwR5jgrltINza0jE&ehbc=2E312F&noprof=1",
         width: 600,
         height: 450
     },
     plastico: {
-        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61769.370622871!2d-90.59315749364247!3d14.62266347220422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a3fe821366b7%3A0xfd632a3b58deb640!2sCiudad%20Cayal%C3%A1!5e0!3m2!1ses!2sgt!4v1712726829641!5m2!1ses!2sgt",
+        url: "https://www.google.com/maps/d/u/0/embed?mid=1jRjjW6VEqU-S1t8NpS6uu9H2eD3NBE0&ehbc=2E312F&noprof=1",
         width: 600,
         height: 450
     }
-    // Agrega más opciones según sea necesario
 };
 
-/// Almacenar referencias a elementos DOM
 const mapa = document.getElementById('mapaGoogle');
 const opcionesCheckbox = document.querySelectorAll('input[type="checkbox"]');
+const buscarBtn = document.getElementById('buscarFiltro');
 
-// Evento de clic en el botón de búsqueda
-document.getElementById('buscarFiltro').addEventListener('click', function(event) {
-    event.preventDefault(); // Evita la acción por defecto del botón (enviar formulario)
+buscarBtn.addEventListener('click', function(event) {
+    event.preventDefault();
 
-    // Obtener la opción seleccionada por el usuario
     const opcionSeleccionada = document.querySelector('input[type="checkbox"]:checked');
 
-    // Limpiar el mapa antes de agregar nuevos marcadores
-    limpiarMapa();
-
-    // Lógica para mostrar ubicaciones en el mapa
     if (opcionSeleccionada) {
-        agregarUbicaciones(opcionSeleccionada.value);
+        limpiarMapa();
+        mostrarUbicacion(opcionSeleccionada.value);
     } else {
         alert('Selecciona una opción para ver la ubicación en el mapa');
     }
 });
 
-// Función para agregar ubicaciones al mapa
-function agregarUbicaciones(opcion) {
-    const ubicacion = mapasPersonalizados[opcion];
-    if (ubicacion) {
-        mapa.src = ubicacion.url;
-        mapa.width = ubicacion.width;
-        mapa.height = ubicacion.height;
-    } else {
-        alert('Ubicación no encontrada');
-    }
-}
-
-// Función para limpiar el mapa (quitar todos los marcadores)
-function limpiarMapa() {
-    mapa.src = ''; // Vacía la URL del mapa
-}
-
-// Evento de cambio en las opciones checkbox
 opcionesCheckbox.forEach(checkbox => {
     checkbox.addEventListener('change', function() {
         if (this.checked) {
-            // Limpiar las demás opciones
             opcionesCheckbox.forEach(otherCheckbox => {
                 if (otherCheckbox !== this) {
                     otherCheckbox.checked = false;
@@ -119,6 +93,20 @@ opcionesCheckbox.forEach(checkbox => {
     });
 });
 
+function limpiarMapa() {
+    mapa.src = '';
+}
+
+function mostrarUbicacion(opcion) {
+    const ubicacion = mapasPersonalizados[opcion];
+    if (ubicacion) {
+        mapa.src = ubicacion.url;
+        mapa.width = ubicacion.width;
+        mapa.height = ubicacion.height;
+    } else {
+        alert('Ubicación no encontrada');
+    }
+}
 
 
 
